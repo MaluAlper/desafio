147ec08b2d6bf833df24c5274e3338b35a39e9a0
Maria Luiza <malualper@hotmail.com>
2020-07-26 21:41:11 -0300

    desafiofinal

---
+++ Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+
+namespace desafio
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+     
+
+        {
+            int x, y, resultado ;
+            string operacao, resp;
+
+
+            Console.Write("Digite um valor: ");
+            x=int.Parse(Console.ReadLine());
+
+           Console.Write("Sinal Operador: ");
+           operacao = Console.ReadLine();
+
+
+            Console.Write("Digite outro valor: ");
+            y=int.Parse(Console.ReadLine());
+
+            switch(operacao)
+            {
+                case "+":
+                resultado=x+y;
+                Console.WriteLine("O valor é: " + resultado);
+                break;
+
+                case "-":
+                resultado=x-y;
+                Console.WriteLine("O valor é: " + resultado);
+                break;
+
+                case "/":
+                resultado=x/y;
+                Console.WriteLine("O valor é: " + resultado);
+                break;
+
+                case "*":
+                resultado=x*y;
+                Console.WriteLine("O valor é: " + resultado);
+                break;
+                
+                case "%":
+                resultado=x*y/100;
+                Console.WriteLine("O valor é: " + resultado);
+                break;
+
+                             
+                       }
+             Console.Write("Deseja continuar? ");
+                resp = Console.ReadLine();
+
+                if (resp == "N")
+                {
+                    Console.ReadKey();
+                }
+                else if
+                
+                 (resp == "S")
+                              
+                {  Console.Write("Digite novamente:" );
+                }
+                return ;
+               
+    
+                }
+                    
+        }
+    }
+
+                    
+
+                     
+                
+            
+
+                
+          
+            
+            
+         
+         
+
+
+                    
+
+
+
+
+        
+    
+
